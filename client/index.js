d9ee0de433f5a0249ba585cc54a1a6e0b87b49bf
Ahad Ali <75221443+ahaadsh@users.noreply.github.com>
2021-03-11 21:41:58 +0300

    Delete client directory

--- client/index.js
+++
@@ -1,8 +0,0 @@
-import { valid_Url } from './js/validUrl'
-import { handleSubmit } from './js/formHandler'
-import './styles/base.scss'
-
-alert("I EXIST");
-console.log("CHANGE!!");
-
-export { handleSubmit,valid_Url };
